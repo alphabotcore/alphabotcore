7c6d7bb9e6acc353d892220c50875d6cb406d22f
Woozy's Studio <woozysstudio23@gmail.com>
2023-12-21 09:59:57 -0600

    Create client setup in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-module.exports = {};
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ],
+    partials: [
+        Partials.Channel,
+        Partials.GuildMember,
+        Partials.Message,
+        Partials.User
+    ]
+});
+
+client.login(process.env.token);
