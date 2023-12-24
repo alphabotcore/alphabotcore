f6a0ae89b4e1e0892ae5fd0fa4b9b31332410071
Woozy's Studio <woozysstudio23@gmail.com>
2023-12-24 12:47:15 -0600

    Create channelinfo command

---
+++ commands/Community/Information/channelinfo.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder, ChatInputCommandInteraction, Client, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("channelinfo")
+        .setDescription("Get info about a channel.")
+        .addChannelOption(option => option.setName('channel').setDescription('Select a channel.').setRequired(true)),
+    /**
+     * @param {ChatInputCommandInteraction} interaction 
+     * @param {Client} client 
+     */
+    async execute(interaction, client) {
+        const channel = interaction.options.getChannel('channel');
+
+        const ChannelEmbed = new EmbedBuilder()
+        .setAuthor({ name: `User Information`, iconURL: client.user.displayAvatarURL() })
+        .setTitle(`About ${channel.name} channel.`)
+        .setColor(0x2B2D31)
+        .addFields(
+            { name: '`📰` Name', value: `${channel.name}`, inline: true },
+            { name: '`🛒` ID', value: `${channel.id}`, inline: true },
+            { name: '`🌟` Created In', value: `<t:${parseInt(channel.createdTimestamp / 1000)}> (<t:${parseInt(channel.createdTimestamp / 1000)}:R>)` },
+        )
+
+        interaction.reply({ embeds: [ChannelEmbed] })
+    }
+}
