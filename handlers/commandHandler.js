3926118277e563a7e0f592b805f80187cac1cce7
Woozy's Studio <woozysstudio23@gmail.com>
2023-12-24 09:21:00 -0600

    Correct blanck spaces

--- handlers/commandHandler.js
+++ handlers/commandHandler.js
@@ -3,34 +3,13 @@ const fs = require("fs");
 require("colors");
 const { join } = require('node:path');
 
-
-
 /**
  * @param {Client} client
  */
 
 function commandListener(client) {
     let commandArray = [];
-
-    /**const path = join(process.cwd(), 'commands');
-    const foldermain = fs.readdirSync(path);
-
-    for (const folders of foldermain) {
-        const path = join(process.cwd(), 'commands', `${folders}`);
-        const files = fs.readdirSync(path).filter((file) => file.endsWith('.js'));
-
-        for (const file of files) {
-            const command = require(`../commands/${folders}/${file}`);
-
-            client.commands.set(command.data.name, command);
-            commandArray.push(command.data.toJSON());
-
-            console.log("[Command] ".yellow + `${command.data.name}`.yellow + ` successfully uploaded.`);
-        }
-
-        client.application.commands.set(commandArray);
-    }*/
-
+    
     const path = join(process.cwd(), 'commands');
     const foldermain = fs.readdirSync(path);
 
--- handlers/eventHandler.js
+++ handlers/eventHandler.js
@@ -3,47 +3,11 @@ const fs = require("fs");
 require("colors");
 const { join } = require('node:path');
 
-
-
 /**
  * @param {Client} client
  */
 
 function eventListener(client) {
-    /**const prinpath = join(process.cwd(), 'events', 'discord');
-    const principal = fs.readdirSync(prinpath);
-
-    for (const folders of principal) {
-
-        const foldersspath = join(process.cwd(), 'events', `discord`);
-        const folderss = fs.readdirSync(foldersspath);
-
-        for (const folder of folderss) {
-            const filespath = join(process.cwd(), 'events', `discord`, `${folder}` );
-            const files = fs.readdirSync(filespath).filter((file) => file.endsWith('.js'));
-
-            for (const file of files) {
-                const event = require(`../events/${folders}/${folder}/${file}`);
-
-                if (event.rest) {
-                    if (event.once)
-                        client.rest.once(event.name, (...args) =>
-                            event.execute(...args, client)
-                        );
-                    else
-                        client.rest.on(event.name, (...args) =>
-                            event.execute(...args, client)
-                        );
-                } else {
-                    if (event.once)
-                        client.once(event.name, (...args) => event.execute(...args, client));
-                    else client.on(event.name, (...args) => event.execute(...args, client));
-                }
-                console.log(`[Event] ${event.name}`.red + ` successfully uploaded.`);
-            }
-        }
-    }*/
-
     const prinpath = join(process.cwd(), 'events', 'discord');
     const principal = fs.readdirSync(prinpath);
 
