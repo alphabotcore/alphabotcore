691b47266af06cbb55e15b4e928dfafc803eca4b
Woozy's Studio <woozysstudio23@gmail.com>
2023-12-24 13:21:37 -0600

    Create botinfo command

---
+++ commands/Community/Information/botinfo.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder, ChatInputCommandInteraction, Client, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("botinfo")
+        .setDescription("Receive information from AlphaBotCore."),
+    /**
+     * @param {ChatInputCommandInteraction} interaction 
+     * @param {Client} client 
+     */
+    async execute(interaction, client) {
+        const BotEmbed = new EmbedBuilder()
+        .setAuthor({ name: `Bot Information`, iconURL: client.user.displayAvatarURL() })
+        .setTitle(`About ${client.user.username}`)
+        .setColor(0x2B2D31)
+        .addFields(
+            { name: '`📹` Name', value: `${client.user.tag}` },
+            { name: '`🛒` ID', value: `${client.user.id}` },
+            { name: '`🌟` Created In', value: `<t:${parseInt(client.user.createdTimestamp / 1000)}> (<t:${parseInt(client.user.createdTimestamp / 1000)}:R>)` },
+            { name: '`🏆` Creator', value: `WoozyStudio <woozystudiocontact@gmail.com>` },
+        )
+        .setThumbnail(client.user.displayAvatarURL({ size: 2048 }))
+
+        interaction.reply({ embeds: [BotEmbed] })
+    }
+}
