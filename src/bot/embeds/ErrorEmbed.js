1fdff7b326d60db57d3f5b5a624dfdb004738f1a
Woozy's Studio <woozysstudio23@gmail.com>
2023-12-24 08:01:15 -0600

    Delete ErrorEmbed.js file

--- src/bot/embeds/ErrorEmbed.js
+++
@@ -1,9 +0,0 @@
-const { EmbedBuilder } = require('discord.js');
-
-const ErrorEmbed = new EmbedBuilder()
-.setTitle("An error has occurred!")
-.setDescription("An error has occurred, check what you are doing!")
-.setColor("Red")
-.setTimestamp()
-
-module.exports = { ErrorEmbed };
