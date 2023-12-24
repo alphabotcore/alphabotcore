261f3b29a0b123081fbe03a544431c532986a1a3
Woozy's Studio <woozysstudio23@gmail.com>
2023-12-24 09:45:38 -0600

    Update buttonHandler

--- events/discord/interactionCreate/interactionCreate.js
+++ events/discord/interactionCreate/interactionCreate.js
@@ -4,14 +4,22 @@ module.exports = {
     name: Events.InteractionCreate,
     /**
      * @param {ChatInputCommandInteraction} interaction 
-     * @param {Client} client 
+     * @param {Client} client
     */
     async execute(interaction, client) {
-        if (!interaction.isChatInputCommand()) return;
+        if (interaction.isChatInputCommand()) {
 
-        const command = client.commands.get(interaction.commandName);
-        if(!command) return interaction.reply({ content: "outdated command" });
+            const command = client.commands.get(interaction.commandName);
+            if (!command) return interaction.reply({ content: "outdated command" });
 
-        command.execute(interaction, client);
+            command.execute(interaction, client);
+
+        } else if (interaction.isButton()) {
+            const button = client.buttons.get(interaction.customId);
+            if (!button) return new Error(`ButtonError: This button does not have a code assigned to it.`)
+
+            await button.execute(interaction, client);
+
+        }
     }
 }
