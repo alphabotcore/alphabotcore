1a316248b193879f61bca7e53ad1bc1cb0db0a5d
Woozy's Studio <woozysstudio23@gmail.com>
2023-12-24 18:22:52 -0600

    Create projects command

---
+++ commands/Community/Information/projects.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder, ChatInputCommandInteraction, Client, EmbedBuilder } = require('discord.js');
+const color = require('../../../tools/Colors.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("projects")
+        .setDescription("Displays AlphaBotCore project list."),
+    /**
+     * @param {ChatInputCommandInteraction} interaction 
+     * @param {Client} client 
+     */
+    async execute(interaction, client) {
+        const ProjectEmbed = new EmbedBuilder()
+        .setColor(color.embed)
+        .setDescription(`
+            **Upcoming Projects:**
+            API
+            Version: 1.0.2
+
+            **Latest Project:**
+            GitHub Project
+        `)
+        .setFooter({ iconURL: client.user.displayAvatarURL(), text: client.user.username })
+        
+        interaction.reply({ embeds: [ProjectEmbed] });
+    }
+}
