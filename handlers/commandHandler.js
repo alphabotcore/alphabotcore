6c67125d90be220cc2eedbb3f521c120525da595
Woozy's Studio <woozysstudio23@gmail.com>
2023-12-26 08:30:34 -0600

    Add discord.js rest system

--- handlers/commandHandler.js
+++ handlers/commandHandler.js
@@ -2,38 +2,49 @@ const { Client } = require("discord.js");
 const fs = require("fs");
 require("colors");
 const { join } = require('node:path');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const config = require('../config/config.json');
 
 /**
  * @param {Client} client
  */
 
-function commandListener(client) {
-    let commandArray = [];
-    
+async function commandListener(client) {
+
     const path = join(process.cwd(), 'commands');
     const foldermain = fs.readdirSync(path);
 
-    for(const folders of foldermain) {
+    for (const folders of foldermain) {
 
         const foldmin = join(process.cwd(), 'commands', `${folders}`);
         const foldermin = fs.readdirSync(foldmin);
 
-        for(const folder of foldermin){
+        for (const folder of foldermin) {
 
             const fil = join(process.cwd(), 'commands', `${folders}`, `${folder}`);
             const files = fs.readdirSync(fil).filter((file) => file.endsWith('.js'));
 
-            for(const file of files) {
+            for (const file of files) {
                 const command = require(`../commands/${folders}/${folder}/${file}`);
 
                 client.commands.set(command.data.name, command);
-                commandArray.push(command.data.toJSON());
+                client.commandArray.push(command.data.toJSON());
 
                 console.log(`[Command] ${command.data.name}`.cyan + ` successfully uploaded.`);
             }
         }
+    }
 
-        client.application.commands.set(commandArray)
+    const rest = new REST({ version: "9" }).setToken(config.bot.token);
+    try {
+        console.log("[Rest] ".green + "Reloaded application (/) commands");
+        
+        await rest.put(Routes.applicationCommands(config.bot.botId), {
+            body: client.commandArray
+        });
+    } catch (error) {
+        console.error(error);
     }
 }
 
