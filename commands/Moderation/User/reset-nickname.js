7eaa0c74ae32bacc4939bd58fa53f3ae8afeb2cd
Woozy's Studio <woozysstudio23@gmail.com>
2023-12-28 22:53:42 -0600

    Create reset-nickname command

---
+++ commands/Moderation/User/reset-nickname.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder, ChatInputCommandInteraction, Client } = require('discord.js');
+const abc = require('alphabotcore');
+const { EmbedBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("reset-nickname")
+        .setDescription("Reset a nickname of a specific user.")
+        .addUserOption(option => option.setName('target').setDescription('The user you are going to reset the nickname.').setRequired(true)),
+    /**
+     * @param {ChatInputCommandInteraction} interaction 
+     * @param {Client} client 
+     */
+    async execute(interaction, client) {
+        const user = interaction.options.getUser('target');
+        const member = await interaction.guild.members.fetch(user.id);
+
+        const NicknameEmbed = new EmbedBuilder()
+        .setColor(abc.colors.green)
+        .setDescription(`${abc.icons.checkmark} The nickname of the user \`@${user.username}\` has been reset successfully.`)
+
+        member.setNickname(null).catch(err => console.log(err));
+
+        interaction.reply({ embeds: [NicknameEmbed], ephemeral: true })
+    }
+}
