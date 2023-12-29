e858d3868b85b0d79ebbcb81b634b2ac679974f8
Woozy's Studio <woozysstudio23@gmail.com>
2023-12-29 09:13:05 -0600

    Create addmod command

---
+++ commands/Moderation/Guild/addmod.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder, ChatInputCommandInteraction, Client, PermissionFlagsBits } = require('discord.js');
+const abc = require('alphabotcore');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("addmod")
+        .setDescription("Add moderator permissions to a role.")
+        .addRoleOption(option => option.setName('role').setDescription('Select the role you want to grant permissions.').setRequired(true)),
+    /**
+     * @param {ChatInputCommandInteraction} interaction 
+     * @param {Client} client 
+     */
+    async execute(interaction, client) {
+        const role = await interaction.options.getRole('role');
+
+        //role.permissions.add(PermissionFlagsBits.BanMembers, PermissionFlagsBits.KickMembers, PermissionFlagsBits.ChangeNickname, PermissionFlagsBits.ManageChannels, PermissionFlagsBits.ManageMessages, PermissionFlagsBits.ManageNicknames, PermissionFlagsBits.ManageRoles, PermissionFlagsBits.ModerateMembers, PermissionFlagsBits.MuteMembers, PermissionFlagsBits.ViewAuditLog)
+
+        role.setPermissions([
+            PermissionFlagsBits.BanMembers,
+            PermissionFlagsBits.KickMembers,
+            PermissionFlagsBits.ChangeNickname,
+            PermissionFlagsBits.ManageChannels,
+            PermissionFlagsBits.ManageMessages,
+            PermissionFlagsBits.ManageNicknames,
+            PermissionFlagsBits.ManageRoles,
+            PermissionFlagsBits.ModerateMembers,
+            PermissionFlagsBits.MuteMembers,
+            PermissionFlagsBits.ViewAuditLog
+        ])
+        interaction.reply({ content: `${abc.icons.checkmark} Added moderator permissions to the role ${role}`, ephemeral: true })
+    }
+}
