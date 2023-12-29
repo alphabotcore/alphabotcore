f080ec0d60571f0174424d82ecdecddd5f756ba2
Woozy's Studio <woozysstudio23@gmail.com>
2023-12-28 19:59:31 -0600

    Create unban command

---
+++ commands/Moderation/User/unban.js
@@ -0,0 +1,36 @@
+const { SlashCommandBuilder, ChatInputCommandInteraction, Client, PermissionFlagsBits, EmbedBuilder } = require('discord.js');
+const core = require('alphabotcore');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("unban")
+        .setDescription("Unbans a user from the server.")
+        .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
+        .addStringOption(option => option.setName('id').setDescription('The member id you want to unban.').setRequired(true))
+        .addStringOption(option => option.setName('reason').setDescription('The reason for unbanning the member.').setRequired(false)),
+    /**
+     * @param {ChatInputCommandInteraction} interaction 
+     * @param {Client} client 
+     */
+    async execute(interaction, client) {
+        const id = interaction.options.getString('id');
+        const reason = interaction.options.getString('reason') || "No reason provided.";
+
+        const UnbanEmbed = new EmbedBuilder()
+        .setColor(core.colors.green)
+        .setDescription(`${core.icons.checkmark} <@${id}> was unbanned with the reason of: ${reason}.`)
+
+        await interaction.guild.bans.fetch().then(async bans => {
+            if(bans.size = 0) return await interaction.reply({ content: `${core.icons.crossmark} There is no one banned from this guild.`, ephemeral: true });
+            let bannedId = bans.find(ban => ban.user.id == id);
+            if(!bannedId) return await interaction.reply({ content: `${core.icons.crossmark} The ID given is not banned from this guild.`, ephemeral: true });
+
+            await interaction.guild.bans.remove(id, reason).catch(err => {
+                return interaction.reply({ content: `${core.icons.crossmark} I don't have permissions to do this action.` });
+            });
+        });
+
+        await interaction.reply({ embeds: [UnbanEmbed] });
+
+    }
+}
